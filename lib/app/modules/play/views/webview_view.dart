9658927ad74c62cffaa0ef63de1df57db7e107e6
d1y <chenhonzhou@gmail.com>
2021-10-28 17:21:11 +0800

    feat: `webview` 修改返回按钮, 打开自动横屏关闭自动竖屏

--- lib/app/modules/play/views/webview_view.dart
+++ lib/app/modules/play/views/webview_view.dart
@@ -1,25 +1,73 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
-import 'package:movie/app/modules/play/controllers/play_controller.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebviewView extends GetView {
+class WebviewView extends StatefulWidget {
+  const WebviewView({Key? key}) : super(key: key);
+
+  @override
+  _WebviewViewState createState() => _WebviewViewState();
+}
+
+class _WebviewViewState extends State<WebviewView> {
+  final url = Get.arguments;
+
+  @override
+  void initState() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
+
+  bool showBackButton = true;
+
+  /// TODO 按钮在点击后三秒内将会 `opacity` => .1
+  // double get opacityValue {
+  //   return showBackButton ? 1.0 : .1;
+  // }
+
+  // late Timer opacityLight;
+
   @override
   Widget build(BuildContext context) {
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-    return GetBuilder<PlayController>(
-      builder: (play) => Scaffold(
-        appBar: AppBar(
-          elevation: 0,
+    return Scaffold(
+      floatingActionButton: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.black38,
+          ),
+          margin: EdgeInsets.symmetric(vertical: 9),
+          child: IconButton(
+            icon: const BackButtonIcon(),
+            color: Colors.white,
+            tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+            onPressed: () {
+              Navigator.maybePop(context);
+            },
+          ),
         ),
-        body: WebView(
-          initialUrl: Get.arguments,
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      body: WebView(
+          initialUrl: url,
           javascriptMode: JavascriptMode.unrestricted,
         ),
-      ),
     );
   }
 }

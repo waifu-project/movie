cfe990f55c3527eda39044e6758a5edcd7e334d1
d1y <chenhonzhou@gmail.com>
2021-11-01 14:50:34 +0800

    feat(settings): 开启 `nsfw`

---
+++ lib/app/modules/home/views/nsfwtable.dart
@@ -0,0 +1,177 @@
+// Copyright (C) 2021 d1y <chenhonzhou@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// 开启 `nsfw`
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:get/get.dart';
+import 'package:movie/app/modules/home/controllers/home_controller.dart';
+
+import 'settings_view.dart';
+
+class NsfwTableView extends StatefulWidget {
+  const NsfwTableView({Key? key}) : super(key: key);
+
+  @override
+  _NsfwTableViewState createState() => _NsfwTableViewState();
+}
+
+class _NsfwTableViewState extends State<NsfwTableView> {
+  final HomeController home = Get.find<HomeController>();
+
+  Future<String> loadNsfwAsset() async {
+    return await rootBundle.loadString('assets/data/nsfw.html');
+  }
+
+  TextEditingController c1 = TextEditingController(text: "");
+  TextEditingController c2 = TextEditingController(text: "");
+
+  String c1Value = "";
+
+  String c2Value = "";
+
+  bool get canInputNext {
+    var _1 = c1Value == '2';
+    var _2 = c2Value == '3';
+    return _1 && _2;
+  }
+
+  String html = "";
+
+  @override
+  void initState() {
+    c1.addListener(() {
+      setState(() {
+        c1Value = c1.text;
+      });
+    });
+    c2.addListener(() {
+      setState(() {
+        c2Value = c2.text;
+      });
+    });
+    loadHtmlCode();
+    super.initState();
+  }
+
+  loadHtmlCode() async {
+    var data = await loadNsfwAsset();
+    setState(() {
+      html = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: CupertinoNavigationBarBackButton(),
+        middle: Text('开启NSFW'),
+      ),
+      child: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Html(
+                data: html,
+              ),
+              Padding(
+                padding: EdgeInsets.all(12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "为了确定您已经未成年, 请完成一道数学题之后开启:  ",
+                      style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.headline6?.fontSize ??
+                                21,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      "某个住在湖边的老人养有狗和鸭子，某天，老人看到5个头、14只脚。那么老人看到的是多少条狗？多少只鸭子？",
+                      style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.bodyText2?.fontSize ??
+                                12,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    Text(
+                      "狗子: ",
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    CupertinoTextField(
+                      controller: c1,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                        color: Get.isDarkMode ? Colors.white : Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "鸭子: ",
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    CupertinoTextField(
+                      controller: c2,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                        color: Get.isDarkMode ? Colors.white : Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    canInputNext
+                        ? Center(
+                            child: CupertinoButton(
+                              color: Colors.red,
+                              child: Text("开启"),
+                              onPressed: () {
+                                Get.back(
+                                  result: GetBackResultType.success,
+                                );
+                              },
+                            ),
+                          )
+                        : SizedBox.shrink(),
+                    SizedBox(
+                      height: Get.height * .42,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/modules/home/views/settings_view.dart
+++ lib/app/modules/home/views/settings_view.dart
@@ -21,7 +21,15 @@ import 'package:get/get.dart';
 import 'package:movie/app/modules/home/controllers/home_controller.dart';
 import 'package:movie/config.dart';
 
-// https://pub.flutter-io.cn/packages/flutter_cupertino_settings
+import 'nsfwtable.dart';
+
+enum GetBackResultType {
+  /// 失败
+  fail,
+
+  /// 成功
+  success
+}
 
 class SettingsView extends StatefulWidget {
   const SettingsView({Key? key}) : super(key: key);
@@ -56,7 +64,7 @@ class _SettingsViewState extends State<SettingsView> {
   }
 
   bool get showNSFW {
-    return home.isNsfw || nShowNSFW >= 10;
+    return (home.isNsfw || nShowNSFW >= 10);
   }
 
   set showNSFW(newVal) {
@@ -75,50 +83,67 @@ class _SettingsViewState extends State<SettingsView> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: CupertinoSettings(items: <Widget>[
-        const CSHeader('常规设置'),
-        CSControl(
-          nameWidget: Text('深色'),
-          contentWidget: CupertinoSwitch(
-            value: isDark,
-            onChanged: (bool value) {
-              isDark = value;
+      body: CupertinoSettings(
+        items: <Widget>[
+          const CSHeader('常规设置'),
+          CSControl(
+            nameWidget: Text('深色'),
+            contentWidget: CupertinoSwitch(
+              value: isDark,
+              onChanged: (bool value) {
+                isDark = value;
+              },
+            ),
+            style: const CSWidgetStyle(
+              icon:
+                  const Icon(Icons.settings_brightness, color: Colors.black54),
+            ),
+          ),
+          showNSFW
+              ? CSControl(
+                  nameWidget: Text('NSFW'),
+                  contentWidget: CupertinoSwitch(
+                    value: home.isNsfw,
+                    onChanged: (bool value) async {
+                      if (value) {
+                        GetBackResultType result =
+                            await Get.to(() => NsfwTableView());
+                        if (result == GetBackResultType.success) {
+                          home.isNsfw = true;
+                          showNSFW = true;
+                          home.update();
+                          return;
+                        }
+                      }
+                      showNSFW = false;
+                      home.isNsfw = false;
+                      home.update();
+                    },
+                  ),
+                  style: const CSWidgetStyle(
+                    icon: const Icon(
+                      Icons.stop_screen_share,
+                      color: Colors.black54,
+                    ),
+                  ),
+                )
+              : SizedBox.shrink(),
+          GestureDetector(
+            onTap: () {
+              if (showNSFW) {
+                showNSFW = false;
+              } else {
+                setState(() {
+                  nShowNSFW++;
+                });
+              }
             },
+            child: CSDescription(
+              "@陈大大哦了",
+            ),
           ),
-          style: const CSWidgetStyle(
-            icon: const Icon(Icons.settings_brightness, color: Colors.black54),
-          ),
-        ),
-        showNSFW
-            ? CSControl(
-                nameWidget: Text('NSFW'),
-                contentWidget: CupertinoSwitch(
-                  value: home.isNsfw,
-                  onChanged: (bool value) {
-                    home.isNsfw = value;
-                  },
-                ),
-                style: const CSWidgetStyle(
-                  icon: const Icon(Icons.stop_screen_share,
-                      color: Colors.black54),
-                ),
-              )
-            : SizedBox.shrink(),
-        GestureDetector(
-          onTap: () {
-            if (showNSFW) {
-              showNSFW = false;
-            } else {
-              setState(() {
-                nShowNSFW++;
-              });
-            }
-          },
-          child: CSDescription(
-            "@陈大大哦了",
-          ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }

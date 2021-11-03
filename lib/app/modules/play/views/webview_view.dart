bf50772961ca8ea8f6c14a1c20874eab91f14d4c
d1y <chenhonzhou@gmail.com>
2021-11-04 00:13:54 +0800

    fix: `webview`  播放全屏卡死
    曲线救国的延时执行就解决了(<或许吧>)

--- lib/app/modules/play/views/webview_view.dart
+++ lib/app/modules/play/views/webview_view.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:auto_orientation/auto_orientation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -18,59 +20,87 @@ class WebviewView extends StatefulWidget {
 class _WebviewViewState extends State<WebviewView> {
   final url = Get.arguments;
 
+  // bool isLand = false;
+
   @override
   void initState() {
     Wakelock.enable();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeRight,
-      DeviceOrientation.landscapeLeft,
-    ]);
+    AutoOrientation.landscapeAutoMode();
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent,);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
     super.initState();
   }
 
   @override
   void dispose() {
-    Wakelock.disable();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
     super.dispose();
+    Wakelock.disable();
+    Timer(Duration(milliseconds: 400), () {
+      AutoOrientation.portraitAutoMode();
+    });
   }
 
-  bool showBackButton = true;
-
-  /// TODO 按钮在点击后三秒内将会 `opacity` => .1
-  // double get opacityValue {
-  //   return showBackButton ? 1.0 : .1;
-  // }
-
-  // late Timer opacityLight;
+  // bool showBackButton = true;
 
   @override
   Widget build(BuildContext context) {
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       floatingActionButton: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.black38,
-          ),
-          margin: EdgeInsets.symmetric(vertical: 9),
-          child: IconButton(
-            icon: const BackButtonIcon(),
-            color: Colors.white,
-            tooltip: MaterialLocalizations.of(context).backButtonTooltip,
-            onPressed: () {
-              Navigator.maybePop(context);
-            },
-          ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.black38,
         ),
+        margin: EdgeInsets.symmetric(vertical: 9),
+        child: IconButton(
+          icon: const BackButtonIcon(),
+          color: Colors.white,
+          tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+          onPressed: () {
+            Navigator.maybePop(context);
+          },
+        ),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-      body: WebView(
-          initialUrl: url,
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: url,
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
+          // Positioned(
+          //   child: GestureDetector(
+          //     child: Container(
+          //       width: 42,
+          //       height: 42,
+          //       decoration: BoxDecoration(
+          //         color: Colors.black,
+          //         borderRadius: BorderRadius.circular(12),
+          //       ),
+          //       child: Icon(
+          //         CupertinoIcons.fullscreen,
+          //         color: Colors.white,
+          //       ),
+          //     ),
+          //     onTap: () {
+          //       // setState(() {
+          //       //   if (isLand) {
+          //       //     isLand = !isLand;
+          //       //     AutoOrientation.portraitUpMode();
+          //       //   } else {
+          //       //     isLand = !isLand;
+          //       //     AutoOrientation.landscapeLeftMode();
+          //       //   }
+          //       // });
+          //     },
+          //   ),
+          //   top: 33,
+          //   right: 18,
+          // )
+        ],
+      ),
     );
   }
 }

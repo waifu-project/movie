b39efbd7c7cc41baf52674ffec1c578dc411426d
d1y <chenhonzhou@gmail.com>
2021-11-10 17:09:34 +0800

    style: remove code

--- lib/app/modules/home/views/index_home_view.dart
+++ lib/app/modules/home/views/index_home_view.dart
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 

d4e1940c3f2bdd07beba9b637a2b0640d872ad17
龙馨竹 <chenhonzhou@gmail.com>
2022-08-14 13:11:50 +0800

    fix: chewie player auto enter fullscreen #20
    #20
    #18

--- lib/app/modules/play/views/chewie_view.dart
+++ lib/app/modules/play/views/chewie_view.dart
@@ -1,12 +1,11 @@
 import 'dart:ui';
 
 import 'package:chewie/chewie.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
-import 'package:movie/app/widget/window_appbar.dart';
-import 'package:movie/utils/screen_helper.dart';
 import 'package:video_player/video_player.dart';
 
 class ChewieView extends StatefulWidget {
@@ -17,90 +16,177 @@ class ChewieView extends StatefulWidget {
 }
 
 class _ChewieViewState extends State<ChewieView> {
-  late VideoPlayerController videoPlayerController;
-
   late ChewieController chewieController;
+  late VideoPlayerController videoPlayerController;
 
   @override
   void initState() {
     super.initState();
-    execScreenDirction(ScreenDirction.x);
-    initializePlayer();
+    init();
   }
 
   @override
   void dispose() {
-    videoPlayerController.dispose();
-    chewieController.dispose();
-    execScreenDirction(ScreenDirction.y);
-    super.dispose();
+    if (!chewieController.isFullScreen) {
+      videoPlayerController.dispose();
+      chewieController.dispose();
+      super.dispose();
+    }
   }
 
   Map<String, dynamic> get args {
     return Get.arguments;
   }
 
-  /// NOTE:
-  ///   => 返回一个 `future`
-  initializePlayer() async {
-    videoPlayerController = VideoPlayerController.network(
-      args['url'],
-    );
-    await videoPlayerController.initialize();
-    chewieController = ChewieController(
+  init() {
+    setState(() {
+      // FIXME: 若没有传入视频地址，则报错
+      videoPlayerController = VideoPlayerController.network(args["url"]);
+    });
+    var controller = ChewieController(
+      optionsTranslation: OptionsTranslation(
+        playbackSpeedButtonText: '播放速度',
+        cancelButtonText: "取消",
+        subtitlesButtonText: "字幕",
+      ),
       videoPlayerController: videoPlayerController,
+      autoInitialize: true,
       autoPlay: true,
+      fullScreenByDefault: true,
+      showControls: true,
+      showControlsOnInitialize: true,
+      allowFullScreen: true,
+      allowedScreenSleep: false,
+      useRootNavigator: false,
       customControls: CupertinoControls(
         backgroundColor: Colors.black38,
         iconColor: Colors.white,
       ),
+      deviceOrientationsAfterFullScreen: [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ],
+      errorBuilder: errorBuilder,
+      placeholder: placeholderWidget,
     );
+
+    bool isInit = true;
+
+    setState(() {
+      chewieController = controller;
+      chewieController.addListener(() {
+        var isFullScreen = chewieController.isFullScreen;
+        if (isFullScreen && isInit) {
+          chewieController.exitFullScreen();
+          setState(() {
+            isInit = false;
+          });
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: WindowAppBar(
-        iosBackStyle: true,
+      // XXX: `chewie` 实际上只支持 `Android` | `iOS`
+      body: SafeArea(
+        child: Chewie(
+          controller: chewieController,
+        ),
       ),
-      body: Chewie(
-        controller: ChewieController(
-          videoPlayerController: videoPlayerController,
-          autoPlay: true,
-          customControls: CupertinoControls(
-            backgroundColor: Colors.black38,
-            iconColor: Colors.white,
+    );
+  }
+
+  Widget errorBuilder(BuildContext context, String errorMessage) {
+    return Center(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 15,
+          sigmaY: 15,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            color: Colors.black.withOpacity(.42),
           ),
-          deviceOrientationsAfterFullScreen: [
-            DeviceOrientation.portraitUp,
-            DeviceOrientation.portraitDown
-          ],
-          placeholder: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 15,
-              sigmaY: 15,
-            ),
-            child: Stack(
-              children: [
-                Image.network(
-                  args['cover'],
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
-                ),
-                Positioned.fill(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                      sigmaX: 15,
-                      sigmaY: 15,
+          padding: EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 24,
+          ).copyWith(
+            bottom: 12,
+          ),
+          width: Get.width * .66,
+          height: Get.height * .28,
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Icon(
+                      CupertinoIcons.bolt_slash_fill,
+                      size: 88,
                     ),
-                    child: Container(
-                      color: Colors.white10,
+                    SizedBox(
+                      height: 12,
                     ),
-                  ),
+                    Text(
+                      "播放失败",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      errorMessage,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              CupertinoButton(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+                color: CupertinoColors.systemRed,
+                child: Text("退出播放"),
+                onPressed: () {
+                  Get.back();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget get placeholderWidget {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black12,
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 15,
+            sigmaY: 15,
+          ),
+          child: Stack(
+            children: [
+              Image.network(
+                args['cover'],
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ],
           ),
         ),
       ),

480627ea16d4e450a323c51787acb7903fbaa3cc
龙馨竹 <chenhonzhou@gmail.com>
2022-08-18 02:13:43 +0800

    perf: 视频源管理长列表使用 `builder`
    长列表的性能优化
    
    Co-Authored-By: 文康 <48707610+Not-See@users.noreply.github.com>

--- lib/app/modules/home/views/mirrortable.dart
+++ lib/app/modules/home/views/mirrortable.dart
@@ -294,59 +294,59 @@ class _MirrorTableViewState extends State<MirrorTableView> {
       child: SafeArea(
         child: Scrollbar(
           controller: scrollController,
-          child: ListView(
+          child: ListView.builder(
             controller: scrollController,
-            children: mirrorList
-                .map(
-                  (e) => mirrorCard(
-                    item: e,
-                    current: home.currentMirrorItem == e,
-                    onTap: () {
-                      var index = mirrorList.indexOf(e);
-                      home.updateMirrorIndex(index);
-                      Get.back();
-                    },
-                    hashTable: __statusMap,
-                    onDel: (context) {
-                      showCupertinoDialog(
-                        builder: (context) => CupertinoAlertDialog(
-                          content: Text("是否删除该镜像源?"),
-                          actions: [
-                            CupertinoDialogAction(
-                              child: const Text(
-                                '我想想',
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                ),
-                              ),
-                              onPressed: () {
-                                Get.back();
-                              },
+            itemCount: mirrorList.length,
+            itemBuilder: (_, index) {
+              var e = mirrorList[index];
+              return mirrorCard(
+                item: e,
+                current: home.currentMirrorItem == e,
+                onTap: () {
+                  var index = mirrorList.indexOf(e);
+                  home.updateMirrorIndex(index);
+                  Get.back();
+                },
+                hashTable: __statusMap,
+                onDel: (context) {
+                  showCupertinoDialog(
+                    builder: (context) => CupertinoAlertDialog(
+                      content: Text("是否删除该镜像源?"),
+                      actions: [
+                        CupertinoDialogAction(
+                          child: const Text(
+                            '我想想',
+                            style: TextStyle(
+                              color: Colors.blue,
                             ),
-                            CupertinoDialogAction(
-                              child: const Text(
-                                '删除',
-                                style: TextStyle(
-                                  color: Colors.red,
-                                ),
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  mirrorList.remove(e);
-                                });
-                                home.removeMirrorItemSync(e);
-                                MirrorManage.removeItem(e);
-                                Get.back();
-                              },
-                            ),
-                          ],
+                          ),
+                          onPressed: () {
+                            Get.back();
+                          },
                         ),
-                        context: context,
-                      );
-                    },
-                  ),
-                )
-                .toList(),
+                        CupertinoDialogAction(
+                          child: const Text(
+                            '删除',
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              mirrorList.remove(e);
+                            });
+                            home.removeMirrorItemSync(e);
+                            MirrorManage.removeItem(e);
+                            Get.back();
+                          },
+                        ),
+                      ],
+                    ),
+                    context: context,
+                  );
+                },
+              );
+            },
           ),
         ),
       ),

04236513826efb659b58b1c3ab18ecf8f4b5c222
d1y <chenhonzhou@gmail.com>
2025-08-02 16:14:16 +0800

    ios add parse

--- lib/app/modules/play/controllers/play_controller.dart
+++ lib/app/modules/play/controllers/play_controller.dart
@@ -333,6 +333,19 @@ document.addEventListener('DOMContentLoaded', function() {
     return true;
   }
 
+  Future<String> parseIframe(String iframe) async {
+    var closed = showLoading("正在解析iframe");
+    var result = await home.currentMirrorItem.parseIframe(iframe);
+    closed();
+    if (result.isEmpty) {
+      EasyLoading.showError("解析失败, 无法播放");
+      return "";
+    }
+    debugPrint("result: $result");
+    String url = result[0]; // NOTE(d1y): 估计解析到不止一个, 该用哪一个呢!
+    return url;
+  }
+
   Future<bool> handleTapPlayerButtom(
     VideoInfo curr,
     List<VideoInfo> playList,
@@ -403,6 +416,10 @@ document.addEventListener('DOMContentLoaded', function() {
               );
             }
           } else {
+            if (curr.type == VideoType.iframe) {
+              url = await parseIframe(url);
+              if (url.isEmpty) return false;
+            }
             url.openURL();
           }
         }
@@ -413,29 +430,15 @@ document.addEventListener('DOMContentLoaded', function() {
           return false;
         }
         if (curr.type == VideoType.iframe) {
-          var closed = showLoading("正在解析iframe");
-          var result = await home.currentMirrorItem.parseIframe(url);
-          closed();
-          if (result.isEmpty) {
-            EasyLoading.showError("解析失败, 无法播放");
-            return false;
-          }
-          debugPrint("result: $result");
-          url = result[0]; // NOTE(d1y): 估计解析到不止一个, 该用哪一个呢!
+          url = await parseIframe(url);
+          if (url.isEmpty) return false;
         }
         url.openToIINA();
         break;
       case VideoKernel.mediaKit:
         if (curr.type == VideoType.iframe) {
-          var closed = showLoading("正在解析iframe");
-          var result = await home.currentMirrorItem.parseIframe(url);
-          closed();
-          if (result.isEmpty) {
-            EasyLoading.showError("解析失败, 无法播放");
-            return false;
-          }
-          debugPrint("result: $result");
-          url = result[0]; // NOTE(d1y): 估计解析到不止一个, 该用哪一个呢!
+          url = await parseIframe(url);
+          if (url.isEmpty) return false;
         }
         mediaKitPlayer.open(Media(url));
         break;
